579ab01aef90ea0cab4e91d18c45c1a7a4eef283
Vikram Modh <vikrammodh0111@gmail.com>
2020-03-22 19:23:22 +0530

    forgot password page code improve

--- portal/src/Pages/ForgotPassword/layout.js
+++ portal/src/Pages/ForgotPassword/layout.js
@@ -1,38 +1,29 @@
 import React, { useState } from "react";
 import useStyles from "./style";
 import Header from "Components/Header";
-import {
-  Container,
-  Typography,
-  Button
-} from "@material-ui/core";
+import { Container, Typography, Button } from "@material-ui/core";
 import Config from "Config";
 import EmailIcon from "@material-ui/icons/Email";
 import { handleError } from "Store/helper";
 import { useHistory } from "react-router-dom";
-import { Snackbar, InputComponent } from "Components";
+import { InputComponent } from "Components";
 import ForgotpasswordIcon from "./Assets/forgot.svg";
 import { NetworkServices } from "Services";
 import { openGlobalMessageBox } from "Helper";
 
 const Layout = () => {
   const classes = useStyles();
-  const history = useHistory();
-  const [success, setSuccess] = useState(false);
   const [username, setUsername] = useState();
-  const [state, setState] = useState({
-    isOpen: false,
-    variant: "error",
-    message: ""
-  });
+  const [submitting,setSubmitting] = useState(false);
 
   const handleSubmit = async e => {
     e.preventDefault();
+    setSubmitting(true);
     if (!username) {
       return openGlobalMessageBox({
-        title:'Doctor AI',
-        message:'Please enter valid email',
-        type:'error',
+        title: "Doctor AI",
+        message: "Please enter valid email",
+        type: "error"
       });
     }
     try {
@@ -40,91 +31,64 @@ const Layout = () => {
         username
       });
       openGlobalMessageBox({
-        title:'Doctor AI',
-        message:'Forgot Password token sent to your registered email id',
-        type:'success',
+        title: "Doctor AI",
+        message:
+          "An Email with Reset intruction Send In Your email Address,Kindly Check Your Email..",
+        type: "success"
       });
     } catch (error) {
-      handleError();
+      handleError(error);
     } finally {
       setUsername("");
+      setSubmitting(false);
     }
   };
 
   return (
     <div>
-      {success ? (
-        <div
-          style={{
-            display: "flex",
-            minHeight: "100vh",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column"
-          }}
-        >
-          <h5 style={{ fontSize: "1.5rem", fontFamily: "Poppins" }}>
-            An Email with Reset intruction Send In Your email Address,Kindly
-            Check Your Email..
-          </h5>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => history.push("/login")}
-          >
-            GO BACK TO LOGIN
-          </Button>
+      <div className={classes.cabbooking}>
+        <Header title="Forgot password" />
+        <div className={classes.cabcontent}>
+          <Container className={classes.container} maxWidth="sm">
+            <div style={{ textAlign: "center" }}>
+              <img
+                src={ForgotpasswordIcon}
+                alt="forgotpasswordicon"
+                className={classes.forgotpasswordIcon}
+              />
+            </div>
+            <form>
+              <Typography
+                variant="h6"
+                align="center"
+                className={classes.title}
+                style={{ marginBottom: "2rem" }}
+              >
+                Verifying Email Please Enter Your Email
+              </Typography>
+              <InputComponent
+                placeholder="Email id"
+                onChange={e => setUsername(e.target.value)}
+                fullWidth
+                size="medium"
+                type="email"
+                Icon={EmailIcon}
+                value={username}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+                style={{ marginTop: 20 }}
+                disabled={!username||submitting}
+                fullWidth
+              >
+                Send Email
+              </Button>
+            </form>
+          </Container>
         </div>
-      ) : (
-        <div className={classes.cabbooking}>
-          <Snackbar
-            errorMessage={state.message}
-            isOpen={state.isOpen}
-            variant={state.variant}
-            handleClose={() => setState({ isOpen: false })}
-          />
-          <Header title="Forgot password" />
-          <div className={classes.cabcontent}>
-            <Container className={classes.container} maxWidth="sm">
-              <div style={{ textAlign: "center" }}>
-                <img
-                  src={ForgotpasswordIcon}
-                  alt="forgotpasswordicon"
-                  className={classes.forgotpasswordIcon}
-                />
-              </div>
-              <form>
-                <Typography
-                  variant="h6"
-                  align="center"
-                  className={classes.title}
-                  style={{ marginBottom: "2rem" }}
-                >
-                  Verifying Email Please Enter Your Email
-                </Typography>
-                <InputComponent
-                  placeholder="Email id"
-                  onChange={e => setUsername(e.target.value)}
-                  fullWidth
-                  size="medium"
-                  type="email"
-                  Icon={EmailIcon}
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleSubmit}
-                  style={{ marginTop: 20 }}
-                  disabled={!username}
-                  fullWidth
-                >
-                  Send Email
-                </Button>
-              </form>
-            </Container>
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
